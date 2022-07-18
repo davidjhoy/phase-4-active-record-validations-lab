1f1a3c2ac4f9544917e1170540e123362c98678a
david <davidjhoyjr@gmail.com>
2022-07-17 20:40:16 -0400

    commit tree c87a796adf6241cbcba31e20e19b1cf8679975e0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, {length: {minimum: 250}}
+    validates :summary, {length: {maximum: 250}}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :title, :title_clickbait_validator
+
+
+    def title_clickbait_validator
+        if title && !title.include?("Won't Believe") && !title.include?("Secret") && !title.include?("Top 10") && !title.include?("Guess")
+            errors.add(:title, "not clickbaity-y enough")
+        end
+    end
+
 end
